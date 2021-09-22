7d15076a103f4d01af4bbe61ba695c94314d49ed
adnanekouna-a <adnanekouna.a@gmail.com>
2021-09-22 11:07:16 +0100

    Conform to PEP-8

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import time
+
 from playsound import playsound
 
 morse_code = {' ':'/', 'A':'.-', 'B':'-...', 'C':'-.-.', 'D':'-..', 'E':'.', 'F':'..-.', 'G':'--.', 'H':'....', 'I':'..', 'J':'.---', 'K':'-.-', 'L':'.-..', 'M':'--', 'N':'-.' , 'O':'---' , 'P':'.--.', 'Q':'--.-', 'R':'.-.', 'S':'...', 'T':'-', 'U':'..-', 'V':'...-', 'W':'.--', 'X':'-..-', 'Y':'-.--', 'Z':'--..', '1':'.----', '2':'..---', '3':'...--', '4':'....-', '5':'.....', '6':'-....', '7':'--...', '8':'---..', '9':'----.', '0':'-----', '?':'..--..', '!':'-.-.--', '.':'.-.-.-', ',':'--..--', ';':'-.-.-.', ':':'---...', '+':'.-.-.', '-':'-....-', '/':'-..-.', '=':'-...-'}
 
-def morsify(text):
+def morsify(text:str) -> str:
+    '''Transforms each character of a given text to its morse equivalent'''
     letters_in_morse = []
-    for letter in text:
-        letters_in_morse.append(morse_code[letter.upper()])
+    for char in text:
+        letters_in_morse.append(morse_code[char.upper()])
     return ''.join(letters_in_morse)
 
-def morse_sound(text):
+def morse_sound(text:str) -> None:
+    '''Translates morse code into sounds'''
     for d in text:
         if d == '.':
             playsound('sounds/dot.wav')
@@ -22,11 +25,11 @@ def morse_sound(text):
 
 print('==>  Welcome to Morse Code Translator (Version 1.0)!  <==')
 print('Enter your text :')
-text = input('-->  ')
+input_text = input('-->  ')
 
 print('The Output:')
-print(f'--> {morsify(text)}')
+print(f'--> {morsify(input_text)}')
 
-for letter in text:
+for letter in input_text:
     morse_sound(morsify(letter))
     time.sleep(0.10)
